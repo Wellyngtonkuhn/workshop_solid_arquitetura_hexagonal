60a2c161dc045543f22c218b97050e39facf29f5
Wellyngton Kuhn <kuhnwellyngton@gmail.com>
2026-07-02 18:19:19 -0300

    - feat: user - adding unity test to update usecase

---
+++ src/modules/users/tests/repositories/InMemoryUserRepository.ts
@@ -0,0 +1,37 @@
+import { User } from "../../domain/entities/User.js";
+import { UserRepository } from "../../domain/repositories/user-repository.js";
+
+export class InMemoryUserRepository implements UserRepository {
+  public users: User[] = [];
+
+  async save(user: User): Promise<User> {
+    this.users.push(user);
+    return user;
+  }
+
+  async update(user: User): Promise<void> {
+    const index = this.users.findIndex(
+      u => u.propsData.id === user.propsData.id
+    );
+
+    this.users[index] = user;
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return this.users.find(
+      u => u.propsData.id === id
+    ) ?? null;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.users.find(
+      u => u.propsData.email === email
+    ) ?? null;
+  }
+
+  async findByPhoneNumber(phone: string): Promise<User | null> {
+    return this.users.find(
+      u => u.propsData.phoneNumber === phone
+    ) ?? null;
+  }
+}
---
+++ src/modules/users/tests/unity/update-user.spec.ts
@@ -0,0 +1,86 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { UpdateUser } from "../../application/useCases/update/index.js";
+import { InMemoryUserRepository } from "../repositories/InMemoryUserRepository.js";
+import { UserNotFound } from "../../errors/user-not-found.error.js";
+import { User, UserStatus } from "../../domain/entities/User.js";
+import { UserCannotUpdateProfileError } from "../../errors/cannot-update-profile.error.js";
+import { VerifiedUserState } from "../../domain/states/user/verified-user.state.js";
+
+let repository: InMemoryUserRepository;
+let sut: UpdateUser;
+
+beforeEach(() => {
+  repository = new InMemoryUserRepository();
+  sut = new UpdateUser(repository);
+});
+
+describe('UseCase - Update user', () => {
+  it('should throw UserNotFound when user does not exist', async () => {
+    await expect((
+      sut.execute('123', {
+        name: "Novo",
+        age: 30,
+        phoneNumber: "+5511999999999",
+        preferredMarketingChannel: "sms",
+      })
+    )).rejects.toBeInstanceOf(UserNotFound)
+  })
+
+  it('should throw UserCannotUpdateProfileError when user can not update his own profile', async () => {
+    const user = User.create({
+      name: "João",
+      age: 20,
+      email: "joao@email.com",
+      password: "123",
+      phoneNumber: "+5511999999999",
+      preferredMarketingChannel: "email",
+    })
+
+    user.propsData.id = "1";
+
+    await repository.save(user);
+    
+    await expect((
+      sut.execute('1', {
+        name: "Novo",
+        age: 30,
+        phoneNumber: "+5511999999999",
+        preferredMarketingChannel: "sms",
+      })
+    )).rejects.toBeInstanceOf(UserCannotUpdateProfileError)
+  })
+
+  it('should update a user', async () => {
+    const user = User.create({
+      name: "João",
+      age: 20,
+      email: "joao@email.com",
+      password: "123",
+      phoneNumber: "+5511999999999",
+      preferredMarketingChannel: "email",
+    });
+
+    user.propsData.id = "3";
+    user.register()
+    user.activate()
+
+    await repository.save(user);
+
+    await sut.execute("3", {
+      name: "Pedro",
+      age: 40,
+      phoneNumber: "+5511988888888",
+      preferredMarketingChannel: "sms",
+    });
+
+    const updated = await repository.findById("3");
+
+    expect(updated?.propsData.name).toBe("Pedro");
+
+    expect(updated?.propsData.age).toBe(40);
+
+    expect(updated?.propsData.phoneNumber).toBe("+5511988888888");
+
+    expect(updated?.propsData.preferredMarketingChannel).toBe("sms");
+  });
+})
