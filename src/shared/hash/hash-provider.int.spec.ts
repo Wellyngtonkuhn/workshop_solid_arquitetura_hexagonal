9755fb45527d7a9753ca60a95753831ead2c4fd1
Wellyngton Kuhn <kuhnwellyngton@gmail.com>
2026-07-02 19:01:23 -0300

    feat: hasProvider - adding compare hash to provider - adding integration tests for hasprovider

---
+++ src/shared/hash/hash-provider.int.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { BcryptHashProvider } from "./hashProvider.js";
+
+describe('Integration test for hash provider', () => {
+  it('should hash a value', async () => {
+    const provider = new BcryptHashProvider()
+    const password = "123456789"
+    const hash = await provider.hash(password)
+    expect(hash).not.toBe(password)
+    expect(hash.length).toBeGreaterThan(20)
+  })
+
+  it('should generate a valid bcrypt hash', async () => {
+    const provider = new BcryptHashProvider()
+    const password = "123456789"
+    const hash = await provider.hash(password)
+    const matches = await provider.compare(password, hash)
+    expect(matches).toBe(true)
+  })
+})
--- src/shared/hash/hashProvider.ts
+++ src/shared/hash/hashProvider.ts
@@ -5,4 +5,8 @@ export class BcryptHashProvider implements HashProvider {
   async hash(value: string): Promise<string> {
     return bcrypt.hash(value, 10)
   }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(value, hash);
+  }
 }
